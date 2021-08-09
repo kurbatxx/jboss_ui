97050ebb81ba9011165a98c7e3e93e25ed5f5af8
Kurbatxx <kurbatxx@gmail.com>
2021-08-09 17:55:34 +0600

    add client list provider

--- lib/provider/data_provider.dart
+++ lib/provider/data_provider.dart
@@ -1,5 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../model/school_client.dart';
 
 final loginCheckboxProvider = StateProvider<bool>((ref) => false);
 final searchDeleteClientCheckboxProvider = StateProvider<bool>((ref) => false);
 final freeCheckboxProvider = StateProvider<bool>((ref) => false);
+
+final clientsList =
+    StateProvider<List<SchoolClient>>((ref) => <SchoolClient>[]);
