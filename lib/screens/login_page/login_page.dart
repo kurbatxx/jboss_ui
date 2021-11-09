e7eba7e69ac87f8346bf4182a7fe1574bdb30adb
kurbatxx <kurbatxx@gmail.com>
2021-11-09 21:29:23 +0600

    add gitignore /windows/flutter/generated

--- lib/screens/login_page/login_page.dart
+++ lib/screens/login_page/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:jboss_ui/freezed/authorization_state.dart';
+
 
 import '../../provider/providers.dart';
 
