22dccf6aedbe8a2f9fd9a2476a595b2f256b77bf
kurbatov <kurbatxx@gmail.com>
2022-05-25 14:50:49 +0600

    create register device screen provider

--- lib/screens/register_device_page/register_device_page.dart
+++ lib/screens/register_device_page/register_device_page.dart
@@ -11,6 +11,7 @@ import 'package:jboss_ui/api/jboss.dart';
 import 'package:jboss_ui/freezed/register_device_screen_state.dart';
 import 'package:jboss_ui/models/register_device/register_device_request.dart';
 import 'package:jboss_ui/models/register_device/register_device_response.dart';
+import 'package:jboss_ui/provider/register_device_screen_provider.dart';
 import 'package:jboss_ui/utils/dev_log.dart';
 
 class RegisterDevicePage extends StatelessWidget {
@@ -47,121 +48,7 @@ enum TextControllersEnum {
   rfidId,
 }
 
-final registerDeviceScreenStateProvider = StateNotifierProvider<
-    RegisterDeviceScreenStateNotifier, RegisterDeviceState>(
-  (ref) => RegisterDeviceScreenStateNotifier(
-    const RegisterDeviceState(
-      clientId: '',
-      rfidId: '',
-      loading: false,
-      register: false,
-      errorMessage: '',
-      successMessage: '',
-      clientMessage: '',
-      devicePosition: 0,
-    ),
-  ),
-);
 
-class RegisterDeviceScreenStateNotifier
-    extends StateNotifier<RegisterDeviceState> {
-  RegisterDeviceScreenStateNotifier(RegisterDeviceState state) : super(state);
-
-  clearState() {
-    state = state.copyWith(
-      clientId: "",
-      successMessage: "",
-      errorMessage: "",
-      clientMessage: "",
-      devicePosition: 0,
-      register: false,
-    );
-  }
-
-  toogle() {
-    state = state.copyWith(register: !state.register);
-  }
-
-  updateTextField(
-      {required TextEditingController textController,
-      required TextControllersEnum textControllersEnum}) {
-    switch (textControllersEnum) {
-      case TextControllersEnum.clientId:
-        state = state.copyWith(
-          clientId: textController.text,
-        );
-        break;
-      case TextControllersEnum.rfidId:
-        state = state.copyWith(
-          rfidId: textController.text,
-        );
-        break;
-    }
-    state = state.copyWith(
-      successMessage: "",
-      errorMessage: "",
-      clientMessage: "",
-    );
-  }
-
-  switchDevice(int position) {
-    state = state.copyWith(
-      devicePosition: position,
-    );
-  }
-
-  void registerDevice({
-    required TextEditingController clientIdController,
-    required TextEditingController rfidIdController,
-    required FocusNode clientIdNode,
-    required BuildContext context,
-  }) async {
-    state = state.copyWith(
-      loading: true,
-      errorMessage: "",
-      successMessage: "",
-    );
-
-    final registerDeviceRequest = RegisterDeviceRequest(
-      clientId: int.parse(state.clientId),
-      rfidId: int.parse(state.rfidId),
-      deviceId: devices[state.devicePosition].value,
-    );
-
-    registerDeviceRequest.toJson().log();
-
-    final registerDeviceResponseString =
-        await compute(JbossApi.computeRegisterDevice, registerDeviceRequest);
-    Map<String, dynamic> registerDeviceResponseMap =
-        jsonDecode(registerDeviceResponseString);
-    RegisterDeviceResponse registerDeviceResponse =
-        RegisterDeviceResponse.fromJson(registerDeviceResponseMap);
-
-    //await Future.delayed(const Duration(seconds: 3));
-
-    state = state.copyWith(register: registerDeviceResponse.register);
-
-    if (state.register) {
-      clientIdController.clear();
-      rfidIdController.clear();
-
-      state = state.copyWith(
-        clientId: clientIdController.text,
-        rfidId: rfidIdController.text,
-        loading: false,
-        successMessage: registerDeviceResponse.resultMessage,
-        clientMessage: registerDeviceResponse.client,
-      );
-      if (!mounted) return;
-      FocusScope.of(context).requestFocus(clientIdNode);
-    } else {
-      state = state.copyWith(
-        errorMessage: registerDeviceResponse.resultMessage,
-        loading: false,
-      );
-    }
-  }
-}
 
 class RfidDevice {
   final int value;
