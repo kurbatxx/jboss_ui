1a8654cc04275619a5226a1c054463b968c58fa0
kurbatov <kurbatxx@gmail.com>
2022-05-25 17:53:41 +0600

    add search function

--- lib/provider/search_page_providers.dart
+++ lib/provider/search_page_providers.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jboss_ui/api/jboss.dart';
 import 'package:jboss_ui/freezed/search_page_state.dart';
+import 'package:jboss_ui/models/search/search_request.dart';
+import 'package:jboss_ui/models/search/search_response.dart';
+import 'package:jboss_ui/utils/dev_log.dart';
 
 final searchPageStateProvider =
     StateNotifierProvider<SearchPageStateNotifer, SearchPageState>(
@@ -8,7 +15,8 @@ final searchPageStateProvider =
       isInitial: true,
       searchString: '',
       showDeleted: false,
-      pageNumber: 0,
+      pageNumber: 1,
+      maxPage: 1,
       clientList: [],
       isLoading: false,
       error: '',
@@ -20,6 +28,68 @@ class SearchPageStateNotifer extends StateNotifier<SearchPageState> {
   SearchPageStateNotifer(
     SearchPageState state,
   ) : super(state);
+
+  showDeletedtoogle() {
+    state = state.copyWith(showDeleted: !state.showDeleted);
+
+    if (state.searchString.isNotEmpty) {
+      search(paginated: false);
+    }
+  }
+
+  setSearchString({required String text}) {
+    state = state.copyWith(searchString: text);
+  }
+
+  search({required bool paginated}) async {
+    if (state.searchString.isEmpty) return;
+    if (paginated && !state.isLoading) {
+      state.copyWith(
+        pageNumber: state.pageNumber + 1,
+      );
+    }
+    state = state.copyWith(
+      isLoading: true,
+      isInitial: false,
+    );
+
+    final searchRequest = SearchRequest(
+      searchString: state.searchString,
+      schoolId: 0,
+      page: state.pageNumber,
+      showDeleted: state.showDeleted,
+    );
+
+    final searchResponseString =
+        await compute(JbossApi.computeSearch, searchRequest);
+    Map<String, dynamic> searchResponseMap = jsonDecode(searchResponseString);
+    SearchResponse searchResponse = SearchResponse.fromJson(searchResponseMap);
+
+    if (searchResponse.error.isNotEmpty) {
+      state.copyWith(
+        error: searchResponse.error,
+        isLoading: false,
+      );
+      return;
+    }
+
+    state = state.copyWith(
+      clientList: state.clientList..addAll(searchResponse.clients),
+      isLoading: false,
+    );
+  }
+
+  clearState() {
+    state = state.copyWith(
+      isInitial: true,
+      searchString: '',
+      showDeleted: false,
+      pageNumber: 1,
+      clientList: [],
+      isLoading: false,
+      error: '',
+    );
+  }
 }
 
 // final 
