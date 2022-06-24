6be7a7ca8a2f46654f7f54bca9d994c0a6f7c138
kurbatov <kurbatxx@gmail.com>
2022-06-24 10:04:11 +0600

    add database connect config toml

--- lib/api/ui.dart
+++ lib/api/ui.dart
@@ -1,7 +1,10 @@
+import 'package:toml/toml.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:jboss_ui/states/connection_page_state.dart';
 import 'package:jboss_ui/states/login_screen_state.dart';
 import 'package:jboss_ui/utils/secure.dart';
+import 'dart:io';
 
 class UiInitialApi {
   static Future<LoginScreenState> getLoginState() async {
@@ -17,6 +20,29 @@ class UiInitialApi {
   }
 
   static Future<ConnectionPageState> getConnetionPageState() async {
+    const configPath = 'config.toml';
+    if (File(configPath).existsSync()) {
+      var document = await TomlDocument.load('config.toml');
+      final config = document.toMap();
+      return ConnectionPageState(
+        hostController: TextEditingController(
+          text: config['bd_connect']['host'],
+        ),
+        databaseNameController: TextEditingController(
+          text: config['bd_connect']['database'],
+        ),
+        portController: TextEditingController(
+          text: config['bd_connect']['port'],
+        ),
+        usernameController: TextEditingController(
+          text: config['bd_connect']['username'],
+        ),
+        passwordController: TextEditingController(
+          text: config['bd_connect']['password'],
+        ),
+      );
+    }
+
     return ConnectionPageState(
       hostController: TextEditingController(
         text: await SecureStorage.instance.getDbHost(),
